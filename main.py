7c8899c14e3ef0b9dd053ed709f9951eb2cfd169
andreluiztur13 <andreluizpsilvatur@gmail.com>
2025-04-21 23:04:18 -0300

    Fiz a inclusão do condicional de idade

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("Olá, Mundo Git!")
+idade = int(input('Digite a sua idade: '))
+if idade >= 18:
+    print('Você é maior de idade. Pode entrar.')
+else:
+    print('Você é menor de idade. Proibida a entrada.')
